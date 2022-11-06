e608f24295fcb93e4b995b70c35879b28075a67c
goncalveshigino <goncalveshigino20@gmail.com>
2022-11-06 16:07:10 +0100

    move duplicated code to the setup method

--- test/data/usecase/remote_athentication_test.dart
+++ test/data/usecase/remote_athentication_test.dart
@@ -14,15 +14,17 @@ void main() {
   RemoteAuthentication sut;
   HttpClientSpy httpClient;
   String url;
+  AuthenticationParams params;
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
   });
 
   test('Should call HttpClient with correct values', () async {
-    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    
     await sut.auth(params);
 
     verifyNever(httpClient.request(
@@ -33,10 +35,7 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
-    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
-    .thenThrow(HttpError.badRequest);
-
-    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body'))).thenThrow(HttpError.badRequest);
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.unexpected));
